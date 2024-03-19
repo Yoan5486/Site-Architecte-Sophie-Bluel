85c24591c842f8ab4a2ed8d4dee89654b147e586
Yoan Cocquebert <yoan.principal@gmail.com>
2024-03-19 15:30:17 +0100

    Test de programme pour rajouter dynamiquement les projets

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/js/home.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/js/home.js
@@ -1,5 +1,31 @@
 console.log ("test")
 
-function getWorks () {
+async function getWorks () {
+    const response = await fetch ("http://localhost:5678/api/works")
 
+    return await response.json ()
 }
+
+async function getCategories () {
+    const response = await fetch ("http://localhost:5678/api/categories")
+
+    return await response.json ()
+}
+
+let dynamicProjects = document.querySelector(".gallery")
+
+let baliseFigure = document.createElement("figure", "img", "figcaption")
+
+baliseFigure.img.setAttribute ("alt", "src")
+
+dynamicProjects.forEach(function getWorks() {
+
+    for (let i = 0; i < array.length; i++) { 
+       
+    }
+
+
+});
+
+console.log (getWorks ())
+console.log(getCategories ())
