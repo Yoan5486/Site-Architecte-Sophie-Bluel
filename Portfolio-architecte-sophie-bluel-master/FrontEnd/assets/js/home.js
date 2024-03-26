eae0c279612b7ba2c4f06a451fb701afbc6baee8
Yoan Cocquebert <yoan.principal@gmail.com>
2024-03-26 16:05:11 +0100

    Ajout de la fonction displayCategories + Création de css pour le bouton

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/js/home.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/js/home.js
@@ -1,10 +1,27 @@
-console.log ("test")
-
 async function getWorks () {
     const response = await fetch ("http://localhost:5678/api/works")
 
     return await response.json ()
 }
+
+async function displayWorks () {
+    const dataWorks = await getWorks ()
+    let containerWorks = document.querySelector(".gallery")
+    for (let i = 0; i < dataWorks.length; i++) { 
+        let baliseFigure = document.createElement("figure")
+        let baliseImg = document.createElement("img")
+        let baliseFigcaption = document.createElement("figcaption")
+        containerWorks.appendChild (baliseFigure) 
+        baliseFigure.appendChild (baliseImg)
+        baliseFigure.appendChild (baliseFigcaption)
+        baliseImg.setAttribute ("src", dataWorks[i].imageUrl)
+        baliseImg.setAttribute("alt", dataWorks[i].title)
+        baliseFigcaption.textContent = dataWorks[i].title
+    }
+    console.log(dataWorks)
+}
+
+displayWorks ()
 
 async function getCategories () {
     const response = await fetch ("http://localhost:5678/api/categories")
@@ -12,20 +29,21 @@ async function getCategories () {
     return await response.json ()
 }
 
-let dynamicProjects = document.querySelector(".gallery")
 
-let baliseFigure = document.createElement("figure", "img", "figcaption")
+async function displayCategories () {
+    const dataCategories = await getCategories ()
+    let containerCategories = document.querySelector("#portfolio")
+    for (let i = 0; i < dataCategories.lenght; i++) {
+        let baliseButton  = document.createElement("button")
+        containerCategories.appendChild(baliseButton)
+        baliseButton.setAttribute("", dataCategories[i].name) 
 
-baliseFigure.img.setAttribute ("alt", "src")
-
-dynamicProjects.forEach(function getWorks() {
-
-    for (let i = 0; i < array.length; i++) { 
-       
     }
+    console.log(dataCategories)
+}
 
+displayCategories ()
 
-});
 
 console.log (getWorks ())
 console.log(getCategories ())
