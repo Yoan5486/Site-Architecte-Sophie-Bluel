a073ef64cdebcae99e5f88be81c8ca7ffad110a7
Yoan Cocquebert <yoan.principal@gmail.com>
2024-04-26 18:20:56 +0200

    Affichage du message d'erreur si le mot de pass et/ou l'identifiant sont incorrects

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/js/login__page.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/js/login__page.js
@@ -8,18 +8,30 @@ btnConnect.addEventListener("click", async () =>{
     console.log(idConnection)
 
 const chargeUtile = JSON.stringify(idConnection)
+try {
+    const response = await fetch ("http://localhost:5678/api/users/login", {
+        method: "POST", 
+        headers: {
+           "Content-Type" : "application/json", 
+           "Access-Control-Allow-Origin" : "*"
+        },
+       body: chargeUtile
+   
+   })
 
-await fetch ("http://localhost:5678/api/users/login", {
-     method: "POST", 
-     headers: {
-        "Content-Type" : "application/json", 
-        "Access-Control-Allow-Origin" : "*"
-     },
-    body: chargeUtile
-
-}).then(function (response) {
-    console.log(response)
-})
-
+   if (response.ok) {
+    console.log("Authentification réussie")
+    
+    } else {
+     const errorMessage = document.createElement("p");
+     errorMessage.textContent = "Identifiant ou/et mot de passe incorrect."
+     errorMessage.style.color = "red"
+     const container = document.querySelector(".error__container")
+     container.innerHTML = ""
+     container.appendChild(errorMessage)
+    }
+} catch (error) {
+console.error("Erreur lors de la requête:", error)
+}
 
 })
